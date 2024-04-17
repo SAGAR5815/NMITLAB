858ac7a9977a2bc48cb888ca49fc86e5b22369a8
SAGAR5815 <100685190+SAGAR5815@users.noreply.github.com>
2024-04-17 12:23:08 +0530

    Update ML_app.py

--- ML_app.py
+++ ML_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-from skelearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
 # loading up the classification model we created
 
 model =DecisionTreeClassifier(criterion='gini',max_depth=8, min_samples_leaf=5, random_state=0)
@@ -23,7 +23,7 @@ def predict(Buying, Maint, Doors, Persons, Lug_boot, Safety):
   elif Safety == 'low':
     safety = 3
   df =pd.DataFrame([Buying, Maint, Doors, Persons, Lug_boot, Safety]),
-      columns=['buying','maint','doors','persons','persons','lug_boot','safty'])
+  columns=['Buying','Maint','\Doors','Persons',,'Lug_boot','Safty'])
 prediction =model.predict([Buying, Maint, Doors, Persons, Lug_boot, Safety])
 return prediction
 
@@ -41,18 +41,18 @@ st.header('Enter the Information of the Car:')
 
  st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
 
- Doors st.number_input('doors:', min_value=1, max_value=4, value=1)
+ Doors = st.number_input('doors:', min_value=1, max_value=4, value=1)
 
  st.text("2-persons = 1 4-persons = 2 more = 3 ")
 
- Persons st.number_input('persons:', min_value=1, max_value=3, value=1)
+ Persons = st.number_input('persons:', min_value=1, max_value=3, value=1)
 
  st.text("small 1 med 2 big = 3")
 
- Lug_boot st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
+ Lug_boot = st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
 
- Safety st.radio('safety:', ('med', 'high', 'low'))
+ Safety = st.radio('safety:', ('med', 'high', 'low'))
 
 if st.button('submit_car_Infos'):
-  cal_eval=predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
-  st.success(f'The Evalution of Car: {cal_eval[0]}')
+    cal_eval=predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
+    st.success(f'The Evalution of Car: {cal_eval[0]}')
